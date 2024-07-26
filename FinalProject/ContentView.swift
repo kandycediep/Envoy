9b231863281748f653b18287b667d52c698badf6
kandycediep <kandycediep@gmail.com>
2024-07-26 10:07:56 -0700

    Initial Commit

---
+++ FinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by Kandyce Diep on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Kandyce Diep on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
