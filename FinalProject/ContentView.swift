9ff643f076616ab24a3f59698c8d3303648a690c
kandycediep <kandycediep@gmail.com>
2024-07-30 08:32:16 -0700

    Trial 2 (background does not work)

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemPurple)
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Welcome to Envoy!")
+            }
         }
         .padding()
     }
